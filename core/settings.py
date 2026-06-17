f714d1fb5a24f239b0543691e473c63c6da1cbb9
DanilSJ <danilchagarnoy@mail.ru>
2026-06-17 11:08:41 +0300

    create settings

---
+++ core/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str = getenv("BOT_TOKEN")
+
+    BASE_URL: str = getenv("BASE_URL")
+    AI_TOKEN: str = getenv("AI_TOKEN")
+
+settings = Settings()
