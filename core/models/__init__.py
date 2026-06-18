048d1b158dcee14c104522f3de9add6fe24a3d24
DanilSJ <danilchagarnoy@mail.ru>
2026-06-18 11:05:36 +0300

    add model user, ai

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,8 +1,12 @@
 __all__ = [
+    "AI",
+    "User",
     "Base",
     "DatabaseHelper",
     "db_helper",
 ]
 
+from .user import User
+from .ai import AI
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
