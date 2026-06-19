2398d9817cd082fb739b20ce9273f20cee9a20c5
DanilSJ <danilchagarnoy@mail.ru>
2026-06-19 07:46:11 +0300

    create model message

---
+++ core/models/message.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base import Base
+
+
+class Message(Base):
+    message: Mapped[str] = mapped_column(Text)
+
+    topic_id: Mapped[int] = mapped_column(ForeignKey("topics.id"))
+
+    topic: Mapped["Topic"] = relationship(
+        "Topic",
+        back_populates="messages"
+    )
