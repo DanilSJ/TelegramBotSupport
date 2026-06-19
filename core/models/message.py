548f54089f73e0c10f75ee05df072c764a73cf58
DanilSJ <danilchagarnoy@mail.ru>
2026-06-19 07:51:19 +0300

    set field on topic_id

--- core/models/message.py
+++ core/models/message.py
@@ -6,6 +6,6 @@ from .base import Base
 class Message(Base):
     message: Mapped[str] = mapped_column(Text)
 
-    topic_id: Mapped[int] = mapped_column(ForeignKey("topics.id"))
+    topic_id: Mapped[int] = mapped_column(ForeignKey("topics.topic_id"))
 
     topic: Mapped["Topic"] = relationship("Topic", back_populates="messages")
