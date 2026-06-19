03c787492de28a8dfad6b59c01ab6dba462dcae8
DanilSJ <danilchagarnoy@mail.ru>
2026-06-19 07:46:24 +0300

    relationship topic and message

--- core/models/topic.py
+++ core/models/topic.py
@@ -1,8 +1,14 @@
 from sqlalchemy import String, BigInteger
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
 
 class Topic(Base):
     name: Mapped[str] = mapped_column(String)
     topic_id: Mapped[int] = mapped_column(BigInteger)
+
+    messages: Mapped[list["Message"]] = relationship(
+        "Message",
+        back_populates="topic",
+        cascade="all, delete-orphan",
+    )
