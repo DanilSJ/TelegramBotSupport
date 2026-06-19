4df4e15a6275222e00164e3f01179b51c50f2189
DanilSJ <danilchagarnoy@mail.ru>
2026-06-19 07:25:46 +0300

    add field GROUP_ID_SUPPORT

--- core/config.py
+++ core/config.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 class Settings(BaseSettings):
     BOT_TOKEN: str = getenv("BOT_TOKEN")
+    GROUP_ID_SUPPORT: int = getenv("GROUP_ID_SUPPORT")
 
     BASE_URL: str = getenv("BASE_URL")
     AI_TOKEN: str = getenv("AI_TOKEN")
 
     DB_ECHO: bool = getenv("DB_ECHO", "False") == "True"
     DB_POOL_NULL = getenv("DB_POOL_NULL", "False") == "True"
-
 
 
 settings = Settings()
