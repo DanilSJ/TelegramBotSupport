b82a29252cc9f91c1d5b538a88a75364611b5ac6
DanilSJ <danilchagarnoy@mail.ru>
2026-06-19 07:14:13 +0300

    add field connect_operator, user_topic_id, operator_topic_id, is_operator, is_admin

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, BigInteger
+from sqlalchemy import String, BigInteger, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
@@ -6,3 +6,10 @@ from .base import Base
 class User(Base):
     username: Mapped[str] = mapped_column(String)
     telegram_id: Mapped[int] = mapped_column(BigInteger)
+    connect_operator: Mapped[bool] = mapped_column(Boolean)
+
+    user_topic_id: Mapped[int] = mapped_column(BigInteger)
+    operator_topic_id: Mapped[int] = mapped_column(BigInteger)
+
+    is_operator: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
