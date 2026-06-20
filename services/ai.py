60ba27f8e4d77211d6bc449b856012addc456070
DanilSJ <danilchagarnoy@mail.ru>
2026-06-20 10:32:18 +0300

    add exception for auth ai

--- services/ai.py
+++ services/ai.py
@@ -1,4 +1,5 @@
 from openai import AsyncClient
+from openai import AuthenticationError
 from core.models import db_helper
 from services.crud import get_ai_use
 
@@ -7,7 +8,7 @@ class AI:
     def __init__(self, prompt: str):
         self.prompt: str = prompt
 
-    async def send(self):
+    async def send(self) -> str | None:
         try:
             async with db_helper.scoped_session_dependency() as session:
                 ai = await get_ai_use(session)
@@ -17,18 +18,22 @@ class AI:
                 base_url=ai.base_url,
             )
 
-            result = await client.chat.completions.create(
-                model=ai.model,
-                messages=[
-                    {"role": "system", "content": ai.system_prompt},
-                    {
-                        "role": "user",
-                        "content": self.prompt,
-                    },
-                ],
-            )
-            response = result.choices[0].message.content
+            try:
+                result = await client.chat.completions.create(
+                    model=ai.model,
+                    messages=[
+                        {"role": "system", "content": ai.system_prompt},
+                        {
+                            "role": "user",
+                            "content": self.prompt,
+                        },
+                    ],
+                )
+                response = result.choices[0].message.content
 
-            return response
+                return response
+            except AuthenticationError:
+                return None
+
         except Exception as e:
             raise e
