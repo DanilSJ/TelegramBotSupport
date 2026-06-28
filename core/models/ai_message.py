3adc53034eabc431394a964acca2c7291c4bb53e
DanilSJ <danilchagarnoy@mail.ru>
2026-06-28 10:09:03 +0300

    fix bug name import

--- core/models/ai_message.py
+++ core/models/ai_message.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Text, ForeignKey, BigInteger
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
 
@@ -9,3 +9,5 @@ class Ai_message(Base):
     ai_message: Mapped[str] = mapped_column(Text, nullable=True)
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    user: Mapped["User"] = relationship("User", back_populates="Ai_message")
